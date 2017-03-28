a1f137b4d977c427d60f0489463662c211bc61b3
TranVanQuy0204 <Tran.Van.Quy@framgia.com>
2017-03-29 00:57:20 +0700

    add webpack devServer

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
 var path = require('path');
-
+console.log(123);
 module.exports = {
   context: path.join(__dirname, "src"),
   devtool: debug ? "inline-sourcemap" : null,
@@ -23,9 +23,19 @@ module.exports = {
     path: __dirname + "/src/",
     filename: "client.min.js"
   },
+  devServer: {
+    contentBase: "src/",
+    inline: true
+  },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
   ],
+  // devServer: {
+  //     contentBase: "src",
+  //     inline: true,
+  //     hot: true,
+  //     progress: true
+  // }
 };
